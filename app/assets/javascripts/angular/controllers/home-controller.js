42ee35c363793f2f5ecfe0f1e6361925f4f3eea7
Parag Dadhaniya <paragdadhaniya@gmail.com>
2015-01-16 12:39:32 -0600

    add matchup dropdown menus

--- app/assets/javascripts/angular/controllers/home-controller.js
+++ app/assets/javascripts/angular/controllers/home-controller.js
@@ -2,5 +2,8 @@ app.controller('HomeController', ['$scope', '$location',
   function($scope, $location) {
     $scope.viewTeams = function() {
       $location.path("/teams/");
+    };
+    $scope.viewAddMatchup = function() {
+      $location.path("/matchup");
     }
 }]);
