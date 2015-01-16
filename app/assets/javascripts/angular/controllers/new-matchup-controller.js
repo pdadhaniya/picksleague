f65f91753432f4a5f7c927747cd6c3f32fc9983c
Parag Dadhaniya <paragdadhaniya@gmail.com>
2015-01-16 17:07:11 -0600

    select week when adding matchup

--- app/assets/javascripts/angular/controllers/new-matchup-controller.js
+++ app/assets/javascripts/angular/controllers/new-matchup-controller.js
@@ -5,6 +5,26 @@ app.controller('NewMatchupController', ['$scope', '$location', 'Team', 'Matchup'
       $location.path('/matchups/')
     }
     $scope.create = function(game) {
-      Matchup.createMatchup({matchup: {week: 1, away_team_id: game.away_team.id, home_team_id: game.home_team.id}});
+      Matchup.createMatchup({matchup: {week: game.week.week, away_team_id: game.away_team.id, home_team_id: game.home_team.id}});
+      console.log(game)
     };
+    $scope.weeks = [
+      {week: 1},
+      {week: 2},
+      {week: 3},
+      {week: 4},
+      {week: 5},
+      {week: 6},
+      {week: 7},
+      {week: 8},
+      {week: 9},
+      {week: 10},
+      {week: 11},
+      {week: 12},
+      {week: 13},
+      {week: 14},
+      {week: 15},
+      {week: 16},
+      {week: 17}
+    ]
   }])
